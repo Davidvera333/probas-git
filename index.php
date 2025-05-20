51e76b6c18253e13e0e5acebff344b7183dc0a35
David Vera <davidveraruiz333@gmail.com>
2025-05-20 11:13:30 +0200

    ampliacion del menu desplegable y aumento de contenido

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
         }
 
 
-        .Introduccion,
-        .Descripcion,
-        .login,
-        .controlpanel,
-        .desplegable {
+        #Introduccion,
+        #Descripcion,
+        #login,
+        #controlpanel,
+        #desplegable {
             background: #fff;
             border-radius: 8px;
             padding: 1.5rem;
@@ -86,9 +86,30 @@
                 <button type="button" class="btn-close mb-3" aria-label="Cerrar" id="cerrarIndiceBtn"></button>
                 <h5>Índice</h5>
                 <ul>
-                    <li><a href="#introduccion" onclick="cerrarIndice()">Introducción</a></li>
-                    <li><a href="#paginas-web-y-usos" onclick="cerrarIndice()">Páginas Web y usos</a></li>
-                    <li><a href="#manejo-interno" onclick="cerrarIndice()">Manejo interno</a></li>
+                    <li><a href="#introduccion" onclick="cerrarIndice()"><b>Introducción</b></a></li>
+                    <ol><!--primeraseccion-->
+                        <li><a href="#Descripcion" onclick="cerrarIndice()">Pantalla de Inicio</a></li>
+                        <li><a href="#login" onclick="cerrarIndice()">Pantalla de Login</a></li>
+                        <li><a href="#controlpanel" onclick="cerrarIndice()">Panel de Control</a></li>
+                    </ol>
+                    <!--segundaseccion-->
+                    <li><a href="#paginas-web-y-usos" onclick="cerrarIndice()"><b>Páginas Web y usos</b></a></li>
+                    <ol>
+                        <li><a href="#paginadelaweb" onclick="cerrarIndice()">Paginas de la web</a></li>
+
+                        <li><a href="#quienes-somos" onclick="cerrarIndice()">Quiénes somos</a></li>
+                        <li><a href="#contacto" onclick="cerrarIndice()">Contacto</a></li>
+                        <li><a href="#catalogo" onclick="cerrarIndice()">Catálogo</a></li>
+                        <li><a href="#dentro-de-catalogo" onclick="cerrarIndice()">Dentro de Catálogo</a></li>
+                    </ol>
+                    <li><a href="#manejo-interno" onclick="cerrarIndice()"><b>Manejo interno</b></a></li>
+                    <ol><!--terceraseccion-->
+                        <li>
+                            <a href="#arbolcarpetas" onclick="cerrarIndice()">Árbol de carpetas</a>
+                        </li>
+
+                        <li><a href="#creacion-de-paginas" onclick="cerrarIndice()">Creación de páginas</a>
+                    </ol>
                 </ul>
             </div>
             <script>
@@ -111,21 +132,21 @@
         </div>
 
         <!-- Descripción general de la web -->
-        <div class="Descripcion">
+        <div id="Descripcion">
             <h3><i><b>Pantalla de Inicio</b></i></h3>
             <p>La primera página que se mostrará será Inicio. En la esquina superior derecha se muestra un botón de inicio de sesión llamado <i><b>Iniciar Sesión</b></i>.</p>
             <img src="assets/pantallainicio.JPG" alt="pantallainicio">
         </div>
 
         <!-- Pantalla de Login -->
-        <div class="login">
+        <div id="login">
             <h3><i><b>Pantalla de Login</b></i></h3>
             <p>Al pulsar el botón, serás redireccionado a una pantalla nueva llamada <i><b>Login</b></i> donde ingresarás las credenciales de administrador.</p>
             <img src="assets/login.JPG" alt="login">
         </div>
 
         <!-- Panel de administración -->
-        <div class="controlpanel">
+        <div id="controlpanel">
             <h3><i><b>Panel de Control</b></i></h3>
             <p>La manera de ver a los usuarios registrados, productos almacenados y comentarios de los usuarios acerca de los productos es mediante la base de datos almacenada en <i>PhpMyAdmin</i>.</p>
             <p><i>Árbol de tablas de la BD</i></p>
@@ -135,7 +156,7 @@
         </div>
 
         <h1 id="paginas-web-y-usos" class="text-center">Páginas web y usos</h1>
-        <div class="desplegable">
+        <div id="paginadelaweb" class="desplegable">
             <h3><i><b>Páginas de la Web</b></i></h3>
             <p>A la izquierda del botón <i>Iniciar Sesión</i>, verá el botón de menú desplegable NAV. Si pulsa en él, se desplegarán los enlaces a las diferentes páginas de la web.</p>
             <img src="assets/botonesnav.JPG" alt="Botones del Nav">
@@ -144,19 +165,19 @@
             <p><i>Una pantalla anteriormente mostrada</i>. Será la primera página que vea el usuario.</p>
             <img src="assets/pantallainicio.JPG" alt="Inicio">
 
-            <h3>Quiénes Somos</h3>
+            <h3 id="quienes-somos">Quiénes Somos</h3>
             <p>En esta pantalla el usuario sabrá acerca de la empresa.</p>
             <img src="assets/who.JPG" alt="quienes-somos">
 
-            <h3>Contacto</h3>
+            <h3 id="contacto">Contacto</h3>
             <p>Donde el usuario puede enviar comentarios o solicitar ciertos servicios. Además, incluye en el pie de página la API de Google Maps, localizando así la empresa.</p>
             <img src="assets/mapsform.JPG" alt="formulario y mapa">
 
-            <h3>Catálogo</h3>
+            <h3 id="catalogo">Catálogo</h3>
             <p>En esta página el usuario podrá ver los diferentes productos que ofrece tu empresa. Están todas las categorías separadas en desplegables. Pulsando en <i>Ver más - ver productos</i>, ingresará a la categoría deseada.</p>
             <img src="assets/catalogo.JPG" alt="catalogo">
 
-            <h3>Dentro de Catálogo</h3>
+            <h3 id="dentro-de-catalogo">Dentro de Catálogo</h3>
             <p><i>Se mostrarán los pasos a seguir para consultar información acerca de un producto.</i></p>
             <ol>
                 <li>Pulsa el botón del producto del cual quiera saber más información <i>Ver Producto</i></li>
@@ -171,152 +192,45 @@
 
         <!-- Manejo interno -->
         <h1 id="manejo-interno" class="text-center">Manejo interno</h1>
-        <h3>Árbol de carpetas y el porqué de cada una</h3>
+        <h3 id="arbolcarpetas">Árbol de carpetas y el porqué de cada una</h3>
         <i>Al menos las más destacables</i>
         <p><i>Árbol de Directorios</i></p>
         <img src="assets/treeDIR.JPG" alt="arbol de carpetas">
+        <!--etoymodificandoestahora --> <!--etoymodificandoestahora -->
+        <h3 id="creacion-de-paginas">Creacion de paginas</h3>
+        <p>En esta parte explicaremos la manera de crear una pagina, añadirla a la web, y hacer que tenga una <i>url</i> amigable.</p>
         <ul>
-            <li><b>Assets</b>
-                <p>- Dentro de esta carpeta estarán alojadas las imágenes, las cuales serán vistas por la base de datos.</p>
-            </li>
-            <li><b>Config</b>
-                <p>- Dentro de esta carpeta estará el archivo Config, que sirve para cambiar el entorno del proyecto ("Development, staging, production"), enlazar a su base de datos y definir la URL que usará la página.</p>
-                <pre><code>
-// Detectar si estás en un entorno de desarrollo (localhost) o producción
-//if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
-// Estás en el entorno local
-//   define('BASE_URL', 'http://localhost/estaticos/');
-// define('ENV', 'development'); // Entorno de desarrollo
-//} else {
-// Estás en el entorno de producción
-//  define('BASE_URL', 'https://www.curso-web.com/');
-//   define('ENV', 'production'); // Entorno de producción
-//}
+            <li><b>Views</b> </li>
+            <p>Esta carpeta sera la que almacenara los archivos que construiran la web, dentro de este arbol de carpetas tenemos <i>Views</i> como directorio principal, el mismo Directorio almacenara Los archivos internos que seran el cuerpo de a web, Dentro , tambien tenemos dos carpetas, <i>Admin</i> y <i>Layouts</i>, <i>Admin</i> almacena los archivos de Inicio de sesion y cierre de sesion de los usuarios,y <i>Layout el header y footer</i>.</p>
 
-//lo que esta comentado arriba es lo anterior, lo no comentado abajo es lo nuevo
+            <img src="assets/views.JPG" alt="contenido de la carpeta views">
+            <p>Comencemos con añadir el nombre de esa nueva pagina al menu desplegable de la web, <i>Menu.php</i> es un archivo que nos ayuda a crear ese menu, mediante html y bucles .</p>
+            <pre><code>
+                
+require_once __DIR__ . '/../../models/Menu.php';
+$menu = new menu(); //creamos  instancia del menu 
+//agregar los elementos del menu
+//invocamos la instancia del menu, 
+//explicacion de que es cada seccion
+$menu->agregarItem("Nombre-en-el-menu", "la-url-que-usara", "Nombre-del-archivo-al-que-ira");
+//codigo de ejemplo 
+$menu->agregarItem("Catalogo", "catalogo", "catalogo");
+                  
 
-// Definir el entorno actual: 'development', 'staging' o 'production'
-define('APP_ENV', 'development'); // Cambiar según el entorno
-// Configuración según el entorno
-switch (APP_ENV) {
-    case 'development':
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
-        define('DB_HOST', 'localhost');
-        define('DB_USER', 'root');
-        define('DB_PASS', '');
-        define('DB_NAME', 'curso_web_david');
-        define('BASE_URL', 'http://cursoweb.local/');
-        break;
-    case 'staging':
-        error_reporting(E_ALL & ~E_NOTICE);
-        ini_set('display_errors', 0);
-        define('DB_HOST', 'localhost:3306');
-        define('DB_USER', 'david_DB');
-        define('DB_PASS', '4w2?h61lC');
-        define('DB_NAME', 'curso_web_david');
-        define('BASE_URL', 'https://magical-burnell.87-106-228-101.plesk.page/estaticos/');
-        break;
-    case 'production':
-        error_reporting(0);
-        ini_set('display_errors', 0);
-        define('DB_HOST', 'localhost:3306');
-        define('DB_USER', 'david_DB');
-        define('DB_PASS', '4w2?h61lC');
-        define('DB_NAME', 'curso_web_david');
-        define('BASE_URL', 'https://magical-burnell.87-106-228-101.plesk.page/');
-        break;
-    default:
-        die('Entorno no configurado correctamente.');
-}
-//crear conexion global a al base de datos
-$conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-//comprobar la conexion
-if ($conexion->connect_error) {
-    die("La conexión ha fallado: " . $conexion->connect_error);
-}
 </code></pre>
-            </li>
-            <li><b>Core</b>
-                <p>- Dentro de Core, tenemos varios archivos. El que usaremos será Router, este archivo nos ayuda a configurar las URLs simples:</p>
-                <pre><code>
-class Router
-{
-    private $routes = [
-        'home' => [
-            'view' => 'home.php',
-            'title' => 'Inicio',
-        ],
-        'quienes-somos' => [
-            'view' => 'quienessomos.php',
-            'title' => 'Quienes somos?¿',
-        ],
-        'contacto' => [
-            'view' => 'contacto.php',
-            'title' => 'Contacto',
-        ],
-        'catalogo' => [
-            'view' => 'Catalogo.php',
-            'title' => 'Catalogo',
-        ],
-        'login' => [
-            'view' => '/login.php',
-            'title' => 'login',
-        ],
-        'dashboard' => [
-            'view' => '/dashboard.php',
-            'title' => 'dashboard',
-        ],
-    ];
 
-    public function getRoute()
-    {
-        $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : 'home';
-        $segments = explode('/', $url);
-        $page = $segments[0];
-
-        if (array_key_exists($url, $this->routes)) {
-            return [
-                'pagina' => $url,
-                'view' => 'views/' . $this->routes[$url]['view'],
-                'titulo_pagina' => $this->routes[$url]['title'],
-                'parametro' => null
-            ];
-        }
-
-        if ($page === 'catalogo' && isset($segments[1]) && $segments[1] === 'categoria' && isset($segments[2])) {
-            return [
-                'pagina' => 'categoria',
-                'view' => 'views/categoria.php',
-                'titulo_pagina' => 'Categoria: ' . ucfirst($segments[2]),
-                'parametro' => $segments[2]
-            ];
-        }
-        if ($page === 'catalogo' && isset($segments[1]) && $segments[1] === 'producto' && isset($segments[2])) {
-            return [
-                'pagina' => 'producto',
-                'view' => 'views/producto.php',
-                'titulo_pagina' => 'Producto ID: ' . $segments[2],
-                'parametro' => $segments[2]
-            ];
-        }
-        return [
-            'pagina' => '404',
-            'titulo_pagina' => 'Página no encontrada',
-            'view' => 'views/404.php',
-            'parametro' => null
-        ];
-    }
-}
-                </code></pre>
-            </li>
-            <li><b>Views</b>
-                <p>- Dentro de <i>admin</i> solo usaremos login y logout.</p>
-                <p>- Dentro de layouts: <i>footer y header</i>.</p>
-                <p>- Los demás archivos que están dentro de <i>views</i> son para las diferentes páginas de la web.</p>
-                <img src="assets/views.JPG" alt="contenido de la carpeta views">
-            </li>
         </ul>
+        <p>Ahora toca hacer las URL amigables, con esto la url de esa pagina no sera por ejemplo <b>Miweb.local/archivo.php</b>, si no que pasara a <b>Miweb.local/archivo</b>, ademas de poder añadir un titulo dentro de la pagina . </p>
+        <p><i>IMG archivo rutas</i></p>
+        <img src="assets/core.JPG" alt="URL-amigables">
+        <p><i>Dentro del mismo archivo tambien se pone lo que va delante, osea el miweb.local/ </i>,El resultado final seria, miweb.local/paginacreada .</p>
+        <pre><code>
+  'lo-que-veremos-en-la-url' => [
+            'view' => 'archivo-que-tendra-la-modificacion',
+            'title' => 'el-titulo-que-aparecera-dentro-del-archivo',
+        ],             
+
+</code></pre>
     </div>
 </body>
 
